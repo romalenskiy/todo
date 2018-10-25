e557134b3d8176e9d3db102dba43debe560777fc
Roman Alenskiy <kuperman88@gmail.com>
2018-10-25 19:05:01 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Materialze core
+import '../node_modules/materialize-css/sass/materialize.scss'
+import '../node_modules/materialize-css/dist/js/materialize.min.js'
+
+// Custom styles
+import './app.scss'
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader, 
+            "css-loader", 
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [
+                  autoprefixer({
+                    browsers:['ie >= 8', 'last 10 version']
+                  })
+                ],
+                sourceMap: true
+              }
+            }
+          ]
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader, 
+            "css-loader", 
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [
+                  autoprefixer({
+                    browsers:['ie >= 8', 'last 4 version']
+                  })
+                ],
+                sourceMap: true
+              }
+            }, 
+            "sass-loader"
+          ]
+        },
+        {
+          test: /\.(png|jpg|svg|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                  name: '[path][name].[ext]',
+                  context: path.resolve(__dirname, "src/")
+              }
+            }
+          ]
+        },
+        { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+        { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+    }),
+    new CopyWebpackPlugin([
+      {from: 'src/img', to: 'img'}
+    ])
+  ]
+};
