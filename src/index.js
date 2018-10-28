7c5d73e43981a9604762a99d341bc0828befa27b
Roman Alenskiy <kuperman88@gmail.com>
2018-10-28 18:29:36 +0300

    Added Foundation grid module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+// Using foundation only for Grid layout
+import './foundation.min.css'
+
 // Materialze core
 import '../node_modules/materialize-css/sass/materialize.scss'
 import '../node_modules/materialize-css/dist/js/materialize.min.js'
 
 // Custom styles
 import './app.scss'
+
+// Main stuff
