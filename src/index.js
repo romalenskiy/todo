c0f31851bf49af72bf802ce19642636e81b499b8
Roman Alenskiy <kuperman88@gmail.com>
2018-10-31 20:47:47 +0300

    Finished Project page part

--- src/index.js
+++ src/index.js
@@ -8,4 +8,10 @@ import '../node_modules/materialize-css/dist/js/materialize.min.js'
 // Custom styles
 import './app.scss'
 
+// Expandable accordion initializing
+const collapsible = document.querySelectorAll('.collapsible.expandable');
+let instances = M.Collapsible.init(collapsible, {
+    accordion: false
+});
+
 // Main stuff
